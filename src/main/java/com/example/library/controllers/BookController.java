722d2dd26c5e3a807db01666fbc76d834653e6d2
Lamark <lamark12ricarte@gmail.com>
2023-08-19 23:02:35 -0300

    feat: delete book

--- src/main/java/com/example/library/controllers/BookController.java
+++ src/main/java/com/example/library/controllers/BookController.java
@@ -1,14 +1,15 @@
 package com.example.library.controllers;
-
 import com.example.library.dto.BookDTO;
 import com.example.library.dto.RequestBookDTO;
 import com.example.library.entities.Book;
 import com.example.library.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/books")
@@ -18,23 +19,29 @@ public class BookController {
     private BookService bookService;
 
     @GetMapping
-    public ResponseEntity findAll(){
-        List<BookDTO> bookDTO = bookService.findAll();
-        return ResponseEntity.ok().body(bookDTO);
+    public ResponseEntity<List<BookDTO>> findAll(){
+        List<BookDTO> book = bookService.findAllBooks();
+        return ResponseEntity.ok().body(book);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity findById(@PathVariable Long id){
-        BookDTO result = bookService.findById(id);
-        return ResponseEntity.ok().body(result);
+    public ResponseEntity<BookDTO> findById(@PathVariable Long id){
+        BookDTO result = bookService.findBookById(id);
+        return ResponseEntity.ok(result);
+
     }
 
     @PostMapping(value = "/new")
-    public ResponseEntity createBook(@RequestBody RequestBookDTO request){
+    public ResponseEntity<Book> createBook(@Validated @RequestBody RequestBookDTO request){
         Book book = new Book(request);
         bookService.createBook(book);
         return ResponseEntity.ok().build();
     }
 
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<String> deleteBook(@PathVariable Long id){
+        bookService.deleteBook(id);
+        return ResponseEntity.ok("Livro excluido com sucesso");
+    }
 }
 
--- src/main/java/com/example/library/services/BookService.java
+++ src/main/java/com/example/library/services/BookService.java
@@ -1,14 +1,16 @@
 package com.example.library.services;
 
+import com.example.exceptions.ResourceNotFoundException;
 import com.example.library.BookRepository;
 import com.example.library.dto.BookDTO;
-import com.example.library.dto.RequestBookDTO;
 import com.example.library.entities.Book;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -19,15 +21,20 @@ public class BookService {
 
 
     @Transactional(readOnly = true)
-    public List<BookDTO> findAll(){
-        List<Book> result = bookRepository.findAll();
+    public List<BookDTO> findAllBooks(){
+        List<Book> result = bookRepository.findAllByActiveTrue();
         return result.stream().map(BookDTO::new).toList();
     }
 
     @Transactional(readOnly = true)
-    public BookDTO findById(Long id){
-        Book book = bookRepository.findById(id).get();
-        return new BookDTO(book);
+    public BookDTO findBookById(Long id){
+        Optional<Book> optionalbook = bookRepository.findById(id);
+        if (optionalbook.isPresent()) {
+            Book book = optionalbook.get();
+            return new BookDTO(book);
+        }else {
+            throw new ResourceNotFoundException("Id nao encontrado!");
+        }
     }
 
     @Transactional
@@ -36,6 +43,19 @@ public class BookService {
             bookRepository.save(book);
         }catch (Exception e){
             throw new RuntimeException("Erro ao criar livro !");
+        }
+    }
+
+    @Transactional
+    public void deleteBook(Long id){
+        Optional<Book> OptionalBook = bookRepository.findById(id);
+
+        if (OptionalBook.isPresent()){
+            Book book = OptionalBook.get();
+            book.setActive(false);
+            bookRepository.save(book);
+        }else {
+            throw new EntityNotFoundException("Livro nao encontrado!");
         }
     }
 }
