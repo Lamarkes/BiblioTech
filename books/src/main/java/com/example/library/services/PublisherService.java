b3eb251265bd1d99f85ea765424442dd023a5c91
Lamarkes <lamark12ricarte@gmail.com>
2024-10-08 15:46:39 -0300

    update LICENSE

---
+++ books/src/main/java/com/example/library/services/PublisherService.java
@@ -0,0 +1,2 @@
+package com.example.library.services;public class PublisherService {
+}
--- books/src/main/resources/db/migration/V3__create-table-book.sql
+++ books/src/main/resources/db/migration/V3__create-table-book.sql
@@ -3,14 +3,14 @@
 CREATE TABLE `tb_book`(
     `id` BIGINT NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(100) NOT NULL,
-    `book_year` INT NOT NULL,
-    `author` VARCHAR(100) NOT NULL,
+    `book_year` DATE NOT NULL,
+    `author_name` VARCHAR(100) NOT NULL,
     `book_value` DECIMAL(10,2) NOT NULL,
     `num_pages` VARCHAR(100) NOT NULL,
     `genre` VARCHAR(100) NOT NULL,
     `rating` DOUBLE NOT NULL,
     `book_active` BOOLEAN NOT NULL DEFAULT TRUE,
-    `publishing_company` VARCHAR(100) NOT NULL,
-    `description` TEXT NOT NULL
-
+    `description` TEXT NOT NULL,
+    `editora_id` BIGINT,
+    CONSTRAINT id FOREIGN KEY (editora_id) REFERENCES tb_publisher(id)
 );
