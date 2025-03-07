ede668fe95f02a8ae33b364aa06015b6de7519ac
Lamarkes <lamark12ricarte@gmail.com>
2025-03-07 00:19:01 -0300

    :recycle: refactor: updated Mapper to BookMapper and new fuction -> updated DTO to Entity

--- books/src/main/java/com/example/library/mapper/BookMapper.java
+++ books/src/main/java/com/example/library/mapper/BookMapper.java
@@ -1,11 +1,13 @@
 package com.example.library.mapper;
 
+import com.example.library.dto.book.BookUpdateDTO;
+import com.example.library.entities.Book;
 import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Mapper {
+public class BookMapper {
 
     private static final ModelMapper mapper = new ModelMapper();
 
@@ -20,5 +22,15 @@ public class Mapper {
             destinationObjects.add(mapper.map(o, destination));
         }
         return destinationObjects;
+    }
+
+
+    public static Book updateBookDtoToEntity(BookUpdateDTO request, Book book){
+
+        book.setTitle(request.getTitle());
+        book.setYear(request.getYear());
+        book.setValue(request.getValue());
+        book.setNumPages(request.getNumPages());
+        return book;
     }
 }
